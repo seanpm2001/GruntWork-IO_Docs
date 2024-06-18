49081d245f754db5ff2d228cd31949f2be6dbb8e
Josh Padnick <josh@gruntwork.io>
2024-06-18 11:47:58 -0700

    Fix prism theme syntax

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,8 +2,8 @@
 // Note: type annotations allow type checking and IDEs autocompletion
 const path = require("path")
 
-const lightCodeTheme = require("prism-react-renderer/themes/github")
-const darkCodeTheme = require("prism-react-renderer/themes/dracula")
+const lightCodeTheme = require("prism-react-renderer").themes.github
+const darkCodeTheme = require("prism-react-renderer").themes.dracula
 const cfg = require("config")
 
 const captionsPlugin = require("./src/plugins/captions")
